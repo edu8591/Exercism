63c1e7df275dc76c06aa106f6ee80f0100f42aca
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-02-16 20:43:45 -0500

    finished exercise passing all tests

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -5,3 +5,26 @@ Write your code for the 'Resistor Color Duo' exercise in this file. Make the tes
 To get started with TDD, see the `README.md` file in your
 `ruby/resistor-color-duo` directory.
 =end
+class ResistorColorDuo
+  attr_reader :colors
+
+
+  COLOR_VALUES = {
+    black: '0',
+    brown: '1',
+    red: '2',
+    orange: '3',
+    yellow: '4',
+    green: '5',
+    blue: '6',
+    violet: '7',
+    grey: '8',
+    white: '9'
+  }
+
+  def self.value(colors_arr)
+    first_band_value = COLOR_VALUES[colors_arr[0].downcase.to_sym]
+    second_band_value = COLOR_VALUES[colors_arr[1].downcase.to_sym]
+    (first_band_value + second_band_value).to_i
+  end
+end
