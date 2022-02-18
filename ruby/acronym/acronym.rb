58851ccb4125d6fd49ece38839b23f789bdce2c3
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-02-17 20:22:44 -0500

    implements function to return acronym ofsimple phrases with no special character

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -5,3 +5,25 @@ Write your code for the 'Acronym' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/acronym` directory.
 =end
+
+#  'Acronym' class is designed to parse a phrase and pick the first letter of each word and return an acronym
+class Acronym
+  def self.abbreviate(phrase)
+    acronym = String.new
+    separate_words(phrase).each { |word| acronym += word.split('').first.upcase }
+    acronym
+  end
+
+  def self.separate_words(phrase)
+    words_array = phrase.split(' ')
+  end
+
+  def self.remove_special_characters(words_array)
+    words_array.each do |word|
+
+    end
+  end
+  private_class_method :separate_words, :remove_special_characters
+end
+
+puts Acronym.abbreviate('first in, first out')
