014a6d38fcc6937a0afc263ada66257ad0b2b770
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-02-17 23:34:49 -0500

    refactor to make method easier to read

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -14,15 +14,12 @@ class Matrix
   end
 
   def generate_matrix_rows(matrix)
-    matrix_arr = matrix.split(/\n/)
-    matrix_arr.map do |string|
-      string.strip.split(/\W/).map(&:to_i)
+    matrix.lines(chomp: true).map do |string|
+      string.strip.split(' ').map(&:to_i)
     end
   end
 
   def generate_matrix_columns
-    rows.map.with_index do |_element, index|
-      rows.map { |row| row[index] }
-    end
+    rows.transpose
   end
 end
