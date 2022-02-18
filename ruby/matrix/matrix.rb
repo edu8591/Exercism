f621be9faa08a507975a416b71f3f5f6dfc2336b
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-02-17 23:17:14 -0500

    implemented function to create rows from string of numbers, and create columns based on prevoously created rows

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -5,3 +5,24 @@ Write your code for the 'Matrix' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/matrix` directory.
 =end
+class Matrix
+  attr_reader :rows, :columns
+
+  def initialize(matrix)
+    @rows = generate_matrix_rows(matrix)
+    @columns = generate_matrix_columns
+  end
+
+  def generate_matrix_rows(matrix)
+    matrix_arr = matrix.split(/\n/)
+    matrix_arr.map do |string|
+      string.strip.split(/\W/).map(&:to_i)
+    end
+  end
+
+  def generate_matrix_columns
+    rows.map.with_index do |_element, index|
+      rows.map { |row| row[index] }
+    end
+  end
+end
