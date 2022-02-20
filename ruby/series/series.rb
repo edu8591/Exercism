5563fb5fc0f76d5d4ec21d23e220dcd4e4e20126
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-02-19 19:44:23 -0500

    finished all tests

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -5,3 +5,23 @@ Write your code for the 'Series' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/series` directory.
 =end
+
+class Series
+  attr_reader :digits
+
+  def initialize(digits)
+    @digits = digits.split('')
+  end
+
+  def slices(size)
+    arr = []
+    i = 0
+    raise ArgumentError if size > digits.length
+
+    while i + size <= digits.length
+      arr << digits[i, size].join
+      i += 1
+    end
+    arr
+  end
+end
