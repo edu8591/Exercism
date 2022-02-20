900f14a9667dc356b5364a22ab80a69118997fa4
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-02-20 18:50:43 -0500

    Creates hamming class with compute class method

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -5,3 +5,12 @@ Write your code for the 'Hamming' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/hamming` directory.
 =end
+class Hamming
+  def self.compute(a, b)
+    raise ArgumentError unless a.chars.length == b.chars.length
+
+    counter = 0
+    a.chars.each_with_index { |letter, i| counter += 1 if letter != b.chars[i] }
+    counter
+  end
+end
