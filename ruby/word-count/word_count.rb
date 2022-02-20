86cfea829ba9b5c8c796067b4bbf51b4d4a13121
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-02-20 16:57:07 -0500

    creates method to split the string without special characters

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -5,3 +5,22 @@ Write your code for the 'Word Count' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/word-count` directory.
 =end
+
+class Phrase
+  attr_reader :words
+
+  def initialize(words)
+    @words = split_words(words)
+  end
+
+  def word_count
+    results = {}
+    words.each { |word| results.key?(word) ? results[word] += 1 : results[word] = 1 }
+    results
+  end
+
+  def split_words(words)
+    no_special_characters = words.downcase.gsub(/[^\w']/, ' ')
+    no_special_characters.split(' ').map { |word| word[0] == "'" && word[-1] == "'" ? word[1...-1] : word }
+  end
+end
