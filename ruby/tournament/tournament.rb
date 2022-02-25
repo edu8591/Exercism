182daa28cc06430366a80f2fd3c50c0b88c71c92
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-02-25 01:20:48 -0500

    define method and pass first test

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,7 +1,6 @@
-=begin
-Write your code for the 'Tournament' exercise in this file. Make the tests in
-`tournament_test.rb` pass.
 
-To get started with TDD, see the `README.md` file in your
-`ruby/tournament` directory.
-=end
+class Tournament
+  def self.tally(input)
+    "Team                           | MP |  W |  D |  L |  P\n"
+  end
+end
