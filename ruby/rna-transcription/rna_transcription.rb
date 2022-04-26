c5d075794e0e8adbdd7ca4307af323cae6316d51
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-04-26 13:38:12 -0500

    Finished exercise

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -5,3 +5,17 @@ Write your code for the 'Rna Transcription' exercise in this file. Make the test
 To get started with TDD, see the `README.md` file in your
 `ruby/rna-transcription` directory.
 =end
+
+class Complement
+  @transcribe = {
+    'G'=> 'C',
+    'C'=> 'G',
+    'T'=> 'A',
+    'A'=> 'U',
+  }
+  def self.of_dna(dna_strand)
+    rna = []
+    dna_strand.split('').each { |nucleotide| rna << @transcribe[nucleotide] }
+      rna.join('')
+  end
+end
