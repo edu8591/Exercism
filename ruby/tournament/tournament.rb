b6b8bac6862d000ce7d43cf980dfa2cb737332d8
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-04-25 21:22:49 -0500

    finished exercise

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,46 +1,107 @@
 
 class Tournament
-  attr_accessor :teams
-  @teams = {}
+
   def self.tally(input)
-    splited_string = input.split(';')
-    splited_string.each_with_index do |item, index|
-      next if index == 2
+    return ScoreBoard.no_input if input == "\n" || input.empty? || input.nil?
 
-      @teams[item] ||= {
-        'MP' => 0,
-        'W' => 0,
-        'D' => 0,
-        'L' => 0,
-        'P' => 0
-      }
-      if splited_string.last == 'win' && index.zero?
-        points_to_winner(item)
-      elsif splited_string.last == 'lose' && index == 1
-        points_to_winner(item)
-      elsif  splited_string.last == 'draw'
-        @teams[item]['MP'] += 1
-        @teams[item]['D'] += 1
-        @teams[item]['P'] += 1
-      else
-        @teams[item]['MP'] += 1
-        @teams[item]['L'] += 1
-      end
-    end
-    reply = ["Team                           | MP |  W |  D |  L |  P"]
+    parsed_input_hash = ScoreBoard.parse_input(input)
+    teams = {}
+    parsed_input_hash[:teams].each { |team| teams[team] = Teams.create }
 
-    @teams.each do |key, value|
-
-      reply << "#{key}              |  #{value['MP']} |  #{value['W']}|  #{value['D']} |  #{value['L']} |  #{value['P']}"
+    parsed_input_hash[:lines].each do |line|
+      splited = line.split(';')
+      ScoreBoard.calculate_scores!(teams, splited)
     end
 
-    reply.map { |line| line + "\n"}.join()
-  end
+    teams = ScoreBoard.sort_high_score(teams)
 
-  def self.points_to_winner(team)
-    @teams[team]['MP'] += 1
-    @teams[team]['W'] += 1
-    @teams[team]['P'] += 3
+    # puts ScoreBoard.print_score(teams)
+    ScoreBoard.print_score(teams)
   end
-
 end
+
+class Teams
+  def self.create
+    element = {}
+    element['mp'] = 0
+    element['w'] = 0
+    element['d'] = 0
+    element['l'] = 0
+    element['p'] = 0
+    element
+  end
+end
+
+class ScoreBoard
+  @headers = "#{'Team'.ljust(31, ' ')}| MP |  W |  D |  L |  P"
+  def self.no_input
+    @headers + "\n"
+  end
+
+  def self.calculate_scores!(teams, splited_line)
+    team1 = splited_line[0]
+    team2 = splited_line[1]
+    outcome = splited_line[2]
+    teams[team1]['mp'] += 1
+    teams[team2]['mp'] += 1
+
+    case outcome
+    when 'win'
+      teams[team1]['w'] += 1
+      teams[team1]['p'] += 3
+      teams[team2]['l'] += 1
+    when 'loss'
+      teams[team2]['w'] += 1
+      teams[team2]['p'] += 3
+      teams[team1]['l'] += 1
+    else
+      teams[team1]['d'] += 1
+      teams[team2]['d'] += 1
+      teams[team1]['p'] += 1
+      teams[team2]['p'] += 1
+    end
+  end
+
+  def self.parse_input(input)
+    output = { teams: [], lines: input.lines.each { |line| line.gsub!("\n", '') } }
+    # p output
+    output[:lines].each do |line|
+      splited_line = line.split(';')
+      output[:teams] << splited_line[0] << splited_line[1]
+    end
+    output[:teams].uniq!
+    output
+  end
+
+  def self.sort_high_score(teams)
+    teams.sort_by { |team, scores|  [scores['p']* -1, team] }
+  end
+
+  def self.print_score(teams)
+    results = [@headers]
+    teams.each do |team, scores|
+      results << "#{team.ljust(31,' ')}|  #{scores['mp']} |  #{scores['w']} |  #{scores['d']} |  #{scores['l']} |  #{scores['p']}"
+    end
+    results.join("\n") + "\n"
+  end
+end
+
+
+# test = {
+#   aucas:{score:1},
+#   macara:{score:1},
+#   emelec:{score:1},
+#   barcelona:{score:5},
+# }
+# test = {
+#   'Aucas' => { scores: { 'p' => 1 } },
+#   'Emelec' => { scores: { 'p' => 2 } },
+#   'LDQ' => { scores: { 'p' => 1 } },
+#   'Barcelona' => { scores: { 'p' => 1 } },
+#   'Macara' => { scores: { 'p' => 1 } }
+# }
+# def sort_high_score(teams)
+#   teams.sort_by { |team, scores| [team, scores[:scores]['p']] }
+# end
+
+# sort_high_score(test)
--- ruby/tournament/tournament_test.rb
+++ ruby/tournament/tournament_test.rb
@@ -17,7 +17,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_a_win_is_three_points_a_loss_is_zero_points
-    skip
+    # skip
     input = <<~INPUT
       Allegoric Alaskans;Blithering Badgers;win
     INPUT
@@ -27,12 +27,11 @@ class TournamentTest < Minitest::Test
       Allegoric Alaskans             |  1 |  1 |  0 |  0 |  3
       Blithering Badgers             |  1 |  0 |  0 |  1 |  0
     TALLY
-
     assert_equal expected, Tournament.tally(input)
   end
 
   def test_a_win_can_also_be_expressed_as_a_loss
-    skip
+    # skip
     input = <<~INPUT
       Blithering Badgers;Allegoric Alaskans;loss
     INPUT
@@ -47,7 +46,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_a_different_team_can_win
-    skip
+    # skip
     input = <<~INPUT
       Blithering Badgers;Allegoric Alaskans;win
     INPUT
@@ -62,7 +61,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_a_draw_is_one_point_each
-    skip
+    # skip
     input = <<~INPUT
       Allegoric Alaskans;Blithering Badgers;draw
     INPUT
@@ -77,7 +76,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_there_can_be_more_than_one_match
-    skip
+    # skip
     input = <<~INPUT
       Allegoric Alaskans;Blithering Badgers;win
       Allegoric Alaskans;Blithering Badgers;win
@@ -93,7 +92,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_there_can_be_more_than_one_winner
-    skip
+    # skip
     input = <<~INPUT
       Allegoric Alaskans;Blithering Badgers;loss
       Allegoric Alaskans;Blithering Badgers;win
@@ -109,7 +108,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_there_can_be_more_than_two_teams
-    skip
+    # skip
     input = <<~INPUT
       Allegoric Alaskans;Blithering Badgers;win
       Blithering Badgers;Courageous Californians;win
@@ -127,7 +126,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_typical_input
-    skip
+    # skip
     input = <<~INPUT
       Allegoric Alaskans;Blithering Badgers;win
       Devastating Donkeys;Courageous Californians;draw
@@ -148,8 +147,9 @@ class TournamentTest < Minitest::Test
     assert_equal expected, Tournament.tally(input)
   end
 
+  # this
   def test_incomplete_competition_not_all_pairs_have_played
-    skip
+    # skip
     input = <<~INPUT
       Allegoric Alaskans;Blithering Badgers;loss
       Devastating Donkeys;Allegoric Alaskans;loss
@@ -169,7 +169,7 @@ class TournamentTest < Minitest::Test
   end
 
   def test_ties_broken_alphabetically
-    skip
+    # skip
     input = <<~INPUT
       Courageous Californians;Devastating Donkeys;win
       Allegoric Alaskans;Blithering Badgers;win
